0c832d659bbc76e7a84bd0d5ef7e89e586e04bc9
Åke Engelbrektson <eson57@users.noreply.github.com>
2016-06-19 07:49:20 +0200

    Create harbour-ostos-sv.ts

---
+++ translations/harbour-ostos-sv.ts
@@ -0,0 +1,241 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv" sourcelanguage="en">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Ostos</source>
+        <translation>Ostos</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Shop</source>
+        <translation>Butik</translation>
+    </message>
+    <message>
+        <source>No items</source>
+        <translation>Inga objekt</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Hjälp</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Edit shops</source>
+        <translation>Redigera butiker</translation>
+    </message>
+    <message>
+        <source>Hide bought</source>
+        <translation>Dölj köpta objekt</translation>
+    </message>
+    <message>
+        <source>Search to buy</source>
+        <translation>Sök i inköpslistan</translation>
+    </message>
+    <message>
+        <source>Clearing</source>
+        <translation>Rensar</translation>
+    </message>
+</context>
+<context>
+    <name>ItemAddPage</name>
+    <message>
+        <source>Search</source>
+        <translation>Sök</translation>
+    </message>
+</context>
+<context>
+    <name>ItemEditPage</name>
+    <message>
+        <source>Accept</source>
+        <translation>Acceptera</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <source>Item Name</source>
+        <translation>Objektnamn</translation>
+    </message>
+    <message>
+        <source>Quantity</source>
+        <translation>Kvantitet</translation>
+    </message>
+    <message>
+        <source>Unit</source>
+        <translation>Enhet</translation>
+    </message>
+    <message>
+        <source>Item Class</source>
+        <translation>Objektklass</translation>
+    </message>
+    <message>
+        <source>Shop</source>
+        <translation>Butik</translation>
+    </message>
+    <message>
+        <source>Edit shops</source>
+        <translation>Redigera butiker</translation>
+    </message>
+</context>
+<context>
+    <name>LineButtonsMenu</name>
+    <message>
+        <source>Hiding Item</source>
+        <translation>Döljer objekt</translation>
+    </message>
+    <message>
+        <source>Deleting Item</source>
+        <translation>Tar bort objekt</translation>
+    </message>
+</context>
+<context>
+    <name>NewShopDialog</name>
+    <message>
+        <source>Add new shop</source>
+        <translation>Lägg till ny butik</translation>
+    </message>
+    <message>
+        <source>Shop name</source>
+        <translation>Butiksnamn</translation>
+    </message>
+</context>
+<context>
+    <name>OldHelpTxt</name>
+    <message>
+        <source>Help</source>
+        <translation>Hjälp</translation>
+    </message>
+    <message>
+        <source>Add new items by swithcing to search page on the right (on first item in the pulldown menu.
+</source>
+        <translation>Lägg till nya objekt genom att växla till söksidan åt höger (första objektet i rullgardinsmenyn).
+</translation>
+    </message>
+    <message>
+        <source>The items have states, indicated by the icon on the left of each shopping list item.
+</source>
+        <translation>Objektens statusindikator är den lilla punktikonen på vänster sida om varje inköpsobjekt.
+</translation>
+    </message>
+    <message>
+        <source>The state can be toggled like by clicking the state icon on the left.
+</source>
+        <translation>Status kan växlas genom att trycka på ikonen till vänster.
+</translation>
+    </message>
+    <message>
+        <source>The context menu has options to hide, edit, flag</source>
+        <translation>I kontextmenyn finns alternativ för att dölja, redigera, märka</translation>
+    </message>
+    <message>
+        <source> increase quantity, decrease quantity and delete the item.
+</source>
+        <translation>öka kvantitet, minska kvantitet och ta bort objekt.
+</translation>
+    </message>
+    <message>
+        <source>Add new items by swithcing to search page on the right on first item in the pulldown menu</source>
+        <translation>Lägg till nya objekt genom att växla till söksidan åt höger (första objektet i rullgardinsmenyn).</translation>
+    </message>
+    <message>
+        <source>The item is listed as to buy</source>
+        <translation>Detta objekt är listat för inköp</translation>
+    </message>
+    <message>
+        <source>The item has been bought</source>
+        <translation>Objektet är köpt</translation>
+    </message>
+    <message>
+        <source>The item is listed as flagged (e.g. find later)</source>
+        <translation>Objektet har listats som märkt (t.ex. köp senare)</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>List refresh interval</source>
+        <translation>Listans uppdateringsintervall</translation>
+    </message>
+    <message>
+        <source>DELETE DATABASE TABLES</source>
+        <translation>TA BORT DATABASTABELLER</translation>
+    </message>
+    <message>
+        <source>DELETING ALL DATA</source>
+        <translation>TAR BORT ALL DATA</translation>
+    </message>
+    <message>
+        <source>Enable WWW help</source>
+        <translation>Aktivera WWW-hjälp</translation>
+    </message>
+    <message>
+        <source>Enable Help file read from Web and use of Google translator for unknown languages</source>
+        <translation>Aktivera online-hjälp och användning av Google translator för okända språk</translation>
+    </message>
+</context>
+<context>
+    <name>ShopPage</name>
+    <message>
+        <source>Accept</source>
+        <translation>Acceptera</translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation>Tillbaka</translation>
+    </message>
+    <message>
+        <source>No items</source>
+        <translation>Inga objekt</translation>
+    </message>
+    <message>
+        <source>Add new shop</source>
+        <translation>Lägg till ny butik</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Redigera</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Ta bort</translation>
+    </message>
+</context>
+<context>
+    <name>Splash</name>
+    <message>
+        <source>Shopping list</source>
+        <translation>Inköpslista</translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation>Fortsätt</translation>
+    </message>
+    <message>
+        <source>Ostos</source>
+        <translation>Ostos</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-ostos</name>
+    <message>
+        <source>Updating</source>
+        <translation>Uppdaterar</translation>
+    </message>
+    <message>
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
